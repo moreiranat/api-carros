800b834e5785c56ae3fb31e0acf3fd5d73320b58
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-13 12:18:35 -0300

    Usando o @PathVariable nas requisições

--- src/main/java/io/github/moreiranat/carros/api/IndexController.java
+++ src/main/java/io/github/moreiranat/carros/api/IndexController.java
@@ -11,9 +11,19 @@ public class IndexController {
         return "Get Spring Boot";
     }
 
-    @GetMapping("/login")
-    public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+    @GetMapping("/login/{login}/senha/{senha}")
+    public String login(@PathVariable("login") String login, @PathVariable("senha") String senha) {
         return "Login: " + login + ", Senha: " + senha;
+    }
+
+    @GetMapping("/carros/{id}")
+    public String getCarroById(@PathVariable("id") Long id) {
+        return "Carro by id " + id;
+    }
+
+    @GetMapping("/carros/tipo/{tipo}")
+    public String getCarroByTipo(@PathVariable("tipo") String tipo) {
+        return "Lista de Carros " + tipo;
     }
 
 }
